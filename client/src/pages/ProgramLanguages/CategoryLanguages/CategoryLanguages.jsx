e0aea9a74c28ed18e8cf574875d27a21208374b5
Tranchinh2901 <chinh99078@donga.edu.vn>
2025-06-08 23:17:20 +0700

    Programming Languages

---
+++ client/src/pages/ProgramLanguages/CategoryLanguages/CategoryLanguages.jsx
@@ -0,0 +1,22 @@
+
+const CategoryLanguages = () => {
+  return (
+    <div>
+        <h2>Category Languages</h2>
+        <p>Explore programming languages by category.</p>
+        {/* Add your category languages content here */}
+        <div>
+            {/* Example content */}
+            <ul>
+            <li>JavaScript</li>
+            <li>Python</li>
+            <li>Java</li>
+            <li>C++</li>
+            <li>Ruby</li>
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default CategoryLanguages
