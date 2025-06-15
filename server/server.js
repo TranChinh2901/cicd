b67d0034b1d34f443795f1c1155eb145f2f11d54
Tranchinh2901 <chinh99078@donga.edu.vn>
2025-06-15 23:19:19 +0700

    update code

--- server/server.js
+++ server/server.js
@@ -29,9 +29,7 @@ app.use('/api/v1/brand/', brandLanguagesRouter);
 app.use('/api/v1/category/',categoryLanguagesRouter);
 app.use('/api/v1/language/', languagesRouter);
 app.use('/api/v1/blog/', blogLanguagesRouter)
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on http://0.0.0.0:${PORT}`);
     console.log(`Local: http://localhost:${PORT}`);
